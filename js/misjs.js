6e79e72d8be2798f5935940630ceeea7994a26d1
Silva Salazar Omar Sebastian <omar_2332@comunidad.unam.mx>
2019-11-18 20:34:58 -0600

    Cambios

---
+++ js/misjs.js
@@ -0,0 +1,22 @@
+function funcion(){
+    var ancla = document.getElementsByClassName('formulario');
+       ancla[0].classList.toggle('desaparece');
+  }
+
+  function funcion2(){
+    var ancla = document.getElementsByClassName('formulario');
+
+    $(ancla[1]).addClass("desaparece");
+
+    $(ancla[0]).removeClass('desaparece');
+  }
+
+function funcion2(){
+        var ancla = document.getElementsByClassName('formulario');
+    
+         $(ancla[0]).addClass("desaparece");
+
+        $(ancla[1]).removeClass('desaparece');
+
+}
+    
